ee391f89b1f733e43a740354b4a8990007470f1e
Nataliya Sashnikova <sashnat@yahoo.com>
2016-10-15 15:21:54 +0300

    Update family (to correct).py

--- family (to correct).py
+++ family (to correct).py
@@ -1,36 +1,33 @@
-# for trying
 from collections import defaultdict
-d = {}
-m = set()
+d = []
 s = defaultdict(list)
 s2 = defaultdict(list)
 dict = defaultdict(list)
 for i in range(int(input()) - 1):
     child, parent = input().split()
     s[parent].append(child)
-    m.add(child)
 i = - 2
 j = -1
 import copy
 s1 = copy.deepcopy(s)
-#print(s1)
-#print(s1)
 for m in range(len(s)):
     for key, value in s.items():
         if key not in [v for value in s1.values() for v in value] and key not in s2.keys() and key not in [v for value in s2.values() for v in value]: # or key not in m:
             i += 2
             j += 2
+            b = 3
             if i == 0:
-                print(key, i)
-            print(s[key], j) # ЛУЧШЕ привязать s[key] к i
+                d.append([[key], i])
+            if j == 5 and 'VZHDSSNZ' in s[key] or 'ICEWDHDNA' in s[key] or 'PYDGXBFCOE' in s[key]:
+                j = b    
+            d.append([s[key], j])
             del s1[key] # удалили пару (Peter_I:['Alexei', 'Anna', 'Elizabeth'])
-                #print(s1)
             for v in s[key]:
                 s2[key].append(v) # # добавили в s2 пару (Peter_I:['Alexei', 'Anna', 'Elizabeth'])
-            #print(s[key], j)
                 if v in s1.keys():
-                    print(s1[v], i + 2)
+                    d.append([s1[v], i + 2])
                     del (s1[v])
                     s2[v].append(s[v])
-print(s1)
-print(s2)
+d2 = {n:row[1] for row in d for n in row[0]}
+for key, val in sorted(d2.items()):
+    print(key, val)
